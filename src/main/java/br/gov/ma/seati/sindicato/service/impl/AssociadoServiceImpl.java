8a01f41fa6208ea87e9b1bfde62a1c5d120b5d03
LukeVersus <lvcramos@hotmail.com>
2020-11-06 00:40:11 -0300

    Adicionada opção de lista e paginação.

--- src/main/java/br/gov/ma/seati/sindicato/service/impl/AssociadoServiceImpl.java
+++ src/main/java/br/gov/ma/seati/sindicato/service/impl/AssociadoServiceImpl.java
@@ -1,14 +1,15 @@
 package br.gov.ma.seati.sindicato.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.ma.seati.sindicato.entity.Associado;
 import br.gov.ma.seati.sindicato.entity.Endereco;
@@ -61,6 +62,15 @@ public class AssociadoServiceImpl extends GenericServiceImpl<Associado, Associad
 	public Page<Associado> findByCpf(int page, String cpf, int size ) {
 		Pageable pageable = PageRequest.of(page, size);
 		return associadoRepository.findByCpf(pageable, cpf);
+	}
+			
+	public Page<Associado> findAll(int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return associadoRepository.findAll(pageable);
+	}
+		
+	public List<Associado> findAll() {
+		return associadoRepository.findAll();
 	}
 	
 	public Associado findByCpf(String cpf) {
